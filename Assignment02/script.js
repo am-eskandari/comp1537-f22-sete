fd9417a60a4d8fe7cc51c525386e81ad068a59c6
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2022-11-15 01:16:44 -0800

    create new file
    -assignment02.html
    -script.js
    -style.js

---
+++ Assignment02/script.js
@@ -0,0 +1,13 @@
+setup = function () {
+    $("#search_btn").click(function () {
+        $.ajax({
+            url:
+                type: "GET",
+            success: function (data) {
+                let start_index = 0;
+                let end_index = 0;
+                for (i = start_index; i < end_index; i++) {
+                    $("main").append();
+                }
+            }
+        });
