17e3d07ca7f7a6580b0524e9fec4c7b4ecabe1c3
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2022-11-17 01:16:25 -0800

    Update script.js

--- Assignment02/script.js
+++ Assignment02/script.js
@@ -1,3 +1,6 @@
+
+
+
 // GLOBAL VARIABLES
 var PAGE_SIZE;
 var CURRENT_PAGE = 1;
@@ -6,7 +9,7 @@ var CURRENT_PAGE = 1;
 display_page = () => {
     $.ajax(
         {
-            url: "https://api.themoviedb.org/3/movie/top_rated?api_key=ed4ef9b0f9bcb9c237ab83a2c2ffb909&language=en-US&page=1",
+            url: "https://api.themoviedb.org/3/search/movie?api_key=783a4fda42e8a87a22631b88fac07624&language=en-US&page=1&include_adult=false&query=x",
             type: "GET",
             success: function (data) {
                 let start_index = PAGE_SIZE * (CURRENT_PAGE - 1);
@@ -14,19 +17,19 @@ display_page = () => {
                 for (i = start_index; i < end_index; i++) {
                     $("main").append(
                         `
-            <div>
-              ${data.results[i].title}
-              <p>
-                ${data.results[i].overview}
-              </p>
-              <img 
-                src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}"
-                style="width: 100%"
-              >
-              <button movieBackdropImageName="${data.results[i].backdrop_path}" class="backdropBtn"> BackDrop Image </button>
-              <hr>
-            </div>
-            `
+                        <div>
+                        ${data.results[i].title}
+                        <p>
+                            ${data.results[i].overview}
+                        </p>
+                        <img 
+                            src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}"
+                            style="width: 100%"
+                        >
+                        <button movieBackdropImageName="${data.results[i].backdrop_path}" class="backdropBtn"> BackDrop Image </button>
+                        <hr>
+                        </div>
+                        `
                     )
                         ;
                 }
@@ -53,6 +56,14 @@ setup = function () {
 
 
 
+    $("body").on("click", ".backdropBtn", function () {
+        console.log(`https://image.tmdb.org/t/p/w500/${$(this).attr('movieBackdropImageName')}`);
+        $("aside").html(
+            `
+        <img src="https://image.tmdb.org/t/p/w500/${$(this).attr('movieBackdropImageName')}"> 
+      `
+        )
+    })
+}
 
-
-    $(document).ready(setup)
+$(document).ready(setup)
